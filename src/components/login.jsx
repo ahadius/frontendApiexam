41d5d1c0bc5904da1990705612ba8e75216612bd
ahadius <92294306+ahadius@users.noreply.github.com>
2023-08-10 19:35:57 +0200

    position refactoring

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import { useContext } from 'react';
-import { useState } from 'react'
-import context from '../context/context.js';
-import './login.css'
+import React from "react";
+import { useContext } from "react";
+import { useState } from "react";
+import context from "../context/context.js";
+import "./login.css";
 const Signin = () => {
   const a = useContext(context);
-  const [username, setusername] = useState('');
-  const [password, setpassword] = useState('')
+  const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
   const login = a.login;
 
   return (
-    <div className='container my-5'>
-
-
-
+    <div className="container my-5">
       <section className="content ">
         <div className="row mt-5">
           <div className="col-md-6 w30">
@@ -24,34 +21,49 @@ const Signin = () => {
               <div className="card-body">
                 <div className="form-group">
                   <label htmlFor="username">UserName</label>
-                  <input type="text" id="username"  placeholder='Enter Username' className="form-control" value={username} onChange={(e) => { setusername(e.target.value) }} />
+                  <input
+                    type="text"
+                    id="username"
+                    placeholder="Enter Username"
+                    className="form-control"
+                    value={username}
+                    onChange={(e) => {
+                      setusername(e.target.value);
+                    }}
+                  />
                 </div>
                 <div className="form-group">
                   <label htmlFor="password">Password</label>
-                  <input type="password" id="password" className="form-control" placeholder='Enter password' value={password} onChange={(e) => { setpassword(e.target.value) }} />
+                  <input
+                    type="password"
+                    id="password"
+                    className="form-control"
+                    placeholder="Enter password"
+                    value={password}
+                    onChange={(e) => {
+                      setpassword(e.target.value);
+                    }}
+                  />
                 </div>
               </div>
             </div>
           </div>
-
         </div>
         <div className="row">
           <div className="col-12 my-3">
-            <button className="btn btn-success purple float-right" onClick={() => {
-              login({ email:username, password });
-            }}>
+            <button
+              className="btn btn-success purple float-right"
+              onClick={() => {
+                login({ email: username, password });
+              }}
+            >
               Signin
             </button>
-
           </div>
         </div>
       </section>
-
-
-
-
     </div>
-  )
-}
+  );
+};
 
-export default Signin
+export default Signin;
